a5217209d8d07d01c87111bd795c8cf7c95ff02f
Winmay <mei-tang@qq.com>
2018-05-01 22:01:14 +0800

    '更新mysql操作'

--- sqlServer/sql.js
+++ sqlServer/sql.js
@@ -71,11 +71,52 @@ var deleteTable = ( sqlName ) => {
 
 /**
 *查找数据
-*@sqlName string  表名
+*@table string  表名
 */
-//得到表中的所有记录
-var fetchAllSqlData = ( splNames ) => {
+//查找表中的所有数据
+var fetchAllSqlData = ( table ) => {
+	var _sql = `select * from ${table};`
+	return query( _sql )
+}
 
+//查找不重复数据
+//@param string 需要查找的参数
+var fetchDistinctSqlData = ( table, param ) => {
+	var _sql = `select distinct ${param} from ${table};`
+	return query( _sql )
+}
+
+//带条件查找数据
+// @condition string 条件
+// 用where关键字来实现，可以使用<>!=等多条件可以使用or、and等 
+var fetchSpecialSqlData = ( table, param, condition ) => {
+	var _sql = `select ${param} from ${table} where ${condition};`
+	return query( _sql )
+}
+
+//搜索
+var fetchSearchSqlData = ( table, value, keywords ) => {
+	var _sql = `select * from ${table} where ${value} like '%${keywords}%';`
+	return query( _sql )
+}
+
+/*
+排序和限制  
+desc和asc是排序关键字，desc是降序、asc是升序排列 ORDER BY 排序,默认是升序  
+select * from emp order by sal;  
+如果排序字段的值一样，则值相同的字段按照第二个排序字段进行排序，如果只有一个排序字段，则相同字段将会无序排序  
+select * from emp order by deptno,sal desc;  
+限制  
+select * from emp order by sal limit 3;  
+//前者是起始偏移量，后者是显示行数  
+select * from emp order by sal limit 1,3;  
+  
+limit 和order by 一起使用来做分页  
+*/
+// 分页数据查找
+var fetchPageSqlData = ( table, page, num ) => {
+  var _sql = `select * from ${table} limit ${(page - 1) * num},${num}; `
+  return query(_sql)
 }
 
 /***增加（插入）数据***/
